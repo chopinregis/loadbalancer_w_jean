91a16fb60dfe65b4bb3c5ac711e0fe00c7b646f4
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-15 13:04:16 -0400

    Update loadbalancer.tf

--- loadbalancer/loadbalancer.tf
+++ loadbalancer/loadbalancer.tf
@@ -1,7 +1,7 @@
-resource "azurerm_resource_group" "example" {
-  name     = "LoadBalancerRG"
-  location = "West Europe"
-}
+//resource "azurerm_resource_group" "example" {
+//  name     = "LoadBalancerRG"
+//  location = "West Europe"
+//}
 
 resource "azurerm_public_ip" "example" {
   name                = "PublicIPForLB"
