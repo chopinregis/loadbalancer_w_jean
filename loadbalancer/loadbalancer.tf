6c725cd6b2077c26077605ba4749f14d5edb2d3a
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-15 13:06:13 -0400

    Update loadbalancer.tf

--- loadbalancer/loadbalancer.tf
+++ loadbalancer/loadbalancer.tf
@@ -5,15 +5,15 @@
 
 resource "azurerm_public_ip" "example" {
   name                = "PublicIPForLB"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
   allocation_method   = "Static"
 }
 
 resource "azurerm_lb" "example" {
   name                = "TestLoadBalancer"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
